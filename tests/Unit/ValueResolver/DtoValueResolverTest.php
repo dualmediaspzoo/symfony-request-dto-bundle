b64d0aa245e56f73d151e6f994df3248238243dc
Witold <47141012+JaWitold@users.noreply.github.com>
2024-07-18 09:00:28 +0200

    Switching ArgumentValueResolverInterface into ValueResolverInterface (#15)
    Co-authored-by: Witold Karaś <wk@k4g.com>

---
+++ tests/Unit/ValueResolver/DtoValueResolverTest.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace DualMedia\DtoRequestBundle\Tests\Unit\ValueResolver;
+
+use DualMedia\DtoRequestBundle\Event\DtoResolvedEvent;
+use DualMedia\DtoRequestBundle\Tests\Fixtures\Model\ResolveDto\SubDto;
+use DualMedia\DtoRequestBundle\Tests\PHPUnit\KernelTestCase;
+use DualMedia\DtoRequestBundle\ValueResolver\DtoValueResolver;
+use PHPUnit\Framework\MockObject\MockObject;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Controller\ValueResolverInterface;
+use Symfony\Component\HttpKernel\ControllerMetadata\ArgumentMetadata;
+
+class DtoValueResolverTest extends KernelTestCase
+{
+    private ValueResolverInterface $service;
+    private MockObject $eventMock;
+
+    protected function setUp(): void
+    {
+        self::bootKernel();
+        $this->eventMock = $this->createMock(EventDispatcherInterface::class);
+        self::$container->set('event_dispatcher', $this->eventMock);
+
+        if (interface_exists(\Symfony\Component\HttpKernel\Controller\ValueResolverInterface::class)) {
+            $this->service = $this->getService(DtoValueResolver::class);
+        }
+    }
+
+    public function testResolve(): void
+    {
+        if (!interface_exists(\Symfony\Component\HttpKernel\Controller\ValueResolverInterface::class)) {
+            $this->assertTrue(true);
+
+            return;
+        }
+
+        $event = $this->deferCallable(function ($event): void {
+            $this->assertInstanceOf(DtoResolvedEvent::class, $event);
+            $this->assertInstanceOf(SubDto::class, $event->getDto());
+        });
+
+        $this->eventMock->expects($this->once())
+            ->method('dispatch')
+            ->willReturnCallback(function (...$args) use ($event) {
+                $event->set($args);
+
+                return $args[0];
+            });
+
+        $request = new Request([], [
+            'value' => 155,
+            'floaty_boy' => 22.5,
+        ]);
+
+        $mock = $this->createMock(ArgumentMetadata::class);
+        $mock->method('getType')
+            ->willReturn(SubDto::class);
+        $mock->method('isNullable')
+            ->willReturn(false);
+
+        /**
+         * @var SubDto $dto
+         *
+         * @psalm-suppress InvalidArgument
+         */
+        $dto = iterator_to_array($this->service->resolve($request, $mock))[0];
+
+        $this->assertInstanceOf(SubDto::class, $dto);
+        $this->assertTrue($dto->isValid());
+        $this->assertEquals(155, $dto->value);
+        $this->assertEquals(22.5, $dto->floatVal);
+        $this->assertTrue($dto->visited('value'));
+        $this->assertTrue($dto->visited('floatVal'));
+    }
+}
