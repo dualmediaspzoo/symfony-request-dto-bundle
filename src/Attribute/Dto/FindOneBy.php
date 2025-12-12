a5e0672c41c384356662c5f921cf3a1c37718b56
pkly <kakolp95@gmail.com>
2025-12-12 15:08:04 +0100

    Fix invalid class

--- src/Attribute/Dto/FindOneBy.php
+++ src/Attribute/Dto/FindOneBy.php
@@ -5,6 +5,7 @@ namespace DualMedia\DtoRequestBundle\Attribute\Dto;
 use DualMedia\DtoRequestBundle\Interface\Attribute\DtoAttributeInterface;
 use DualMedia\DtoRequestBundle\Interface\Attribute\FindInterface;
 use DualMedia\DtoRequestBundle\Traits\Annotation\FieldTrait;
+use DualMedia\DtoRequestBundle\Traits\Annotation\LimitAndOffsetTrait;
 use DualMedia\DtoRequestBundle\Traits\Annotation\ProviderTrait;
 use Symfony\Component\Validator\Constraint;
 
@@ -13,6 +14,7 @@ class FindOneBy implements FindInterface, DtoAttributeInterface
 {
     use FieldTrait;
     use ProviderTrait;
+    use LimitAndOffsetTrait;
 
     /**
      * @param array<string, string> $fields
@@ -32,6 +34,7 @@ class FindOneBy implements FindInterface, DtoAttributeInterface
         string|null $errorPath = null,
         array $descriptions = [],
         string|null $provider = null,
+        int|null $offset = null,
         array $static = []
     ) {
         $this->fields = $fields;
@@ -41,6 +44,7 @@ class FindOneBy implements FindInterface, DtoAttributeInterface
         $this->errorPath = $errorPath;
         $this->descriptions = $descriptions;
         $this->provider = $provider;
+        $this->offset = $offset;
         $this->static = $static;
     }
 
