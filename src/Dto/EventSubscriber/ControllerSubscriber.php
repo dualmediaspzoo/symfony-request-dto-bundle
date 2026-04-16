863fcb5b9c422e19170d5b0e5a23edddd3c810cb
pkly <kakolp95@gmail.com>
2026-04-16 19:26:13 +0200

    start work on action stuff

---
+++ src/Dto/EventSubscriber/ControllerSubscriber.php
@@ -0,0 +1,77 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DualMedia\DtoRequestBundle\Dto\EventSubscriber;
+
+use DualMedia\DtoRequestBundle\Dto\AbstractDto;
+use DualMedia\DtoRequestBundle\Dto\Event\DtoInvalidEvent;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\ControllerArgumentsEvent;
+
+class ControllerSubscriber implements EventSubscriberInterface
+{
+    public function __construct(
+        private readonly EventDispatcherInterface $dispatcher
+    )
+    {
+    }
+
+    #[\Override]
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            ControllerArgumentsEvent::class => ['onArguments', 5],
+        ];
+    }
+
+    public function onArguments(
+        ControllerArgumentsEvent $event
+    ): void {
+        /** @var array<int, AbstractDto> $arguments */
+        $arguments = array_filter(
+            $event->getArguments(),
+            static fn ($o) => $o instanceof AbstractDto
+        );
+
+        if (empty($arguments)) {
+            return;
+        }
+
+        $request = $event->getRequest();
+        $requestType = $event->getRequestType();
+        /** @var list<AbstractDto> $invalid */
+        $invalid = [];
+
+        foreach ($arguments as $argument) {
+            $valid = $argument->isValid();
+
+            if ($valid && null !== ($action = $argument->getHttpAction())) {
+                $output = $this->dispatcher->dispatch(new DtoActionEvent($action, $argument, $request, $requestType));
+
+                if (null !== ($response = $output->getResponse())) {
+                    $event->setController(static fn () => $response);
+
+                    return;
+                }
+            }
+
+            if ($valid || $argument->isOptional()) {
+                continue;
+            }
+
+            $invalid[] = $argument;
+        }
+
+        if (empty($invalid)) {
+            return;
+        }
+
+        $output = $this->dispatcher->dispatch(new DtoInvalidEvent($invalid, $request, $requestType));
+
+        if (null !== ($response = $output->getResponse())) {
+            $event->setController(static fn () => $response);
+        }
+    }
+}
